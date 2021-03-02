52fc9fb66f1cfb75a9a96fe02c7d42f22564a737
kunholee_98 <dlrjsgh750@naver.com>
2021-03-03 00:11:05 +0900

    큰 수의 법칙 주석 추가

--- chapter3/law_of_large_num.py
+++ chapter3/law_of_large_num.py
@@ -1,4 +1,4 @@
-# 실전 1-1. 동빈이의 큰 수의 법칙
+# 실전 1-1. 동빈이의 큰 수의 법칙: N 길이의 리스트를 입력받아 M 만큼 숫자를 더해준다. 이때 연속해서 같은 숫자는 K번만 더할 수 있을 때, 최대값은?
 N,M,K = map(int,input(">> ").split())
 num_list = sorted(list(map(int,input(">> ").split())), reverse = True)
 result = 0
