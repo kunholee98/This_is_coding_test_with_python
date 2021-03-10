f8acba55fd682902bbbb6d9b360d458667b3dc4c
kunholee_98 <dlrjsgh750@naver.com>
2021-03-11 01:44:58 +0900

    부품 찾기

--- chapter7/find_component.py
+++ chapter7/find_component.py
@@ -1,24 +1,26 @@
 # 실전 7-1. 부품 찾기: N개의 부품을 가지고 있을 때, M개의 부품 요청에 맞는 부품들이 있는지 확인하여 y/n로 대답하기.
-def binary_tree(num, lst, start, end, mid):
-    print(num,start,end,mid)
+def binary_tree(num, lst, start, end):  # 입력받은 n_lst의 크기가 1000000까지 커질 수 있으므로 sort와 binary_tree 조합으로 구하였음.
+    mid = (start+end)//2
+    #print(num,start,end,mid)
     if num == lst[mid]:
         return "yes"
-    elif start == end:
+    elif start > end:
         return "no"
     elif num > lst[mid]:
-        print("num > mid")
-        print(num,lst[mid])
-        start = mid
-        mid = (start+end)//2+1
-        return binary_tree(num,lst,start,end,mid)
+        #print("num > mid")
+        #print(num,lst[mid])
+        return binary_tree(num,lst,mid+1,end,)
     elif num < lst[mid]:
-        print("num < mid")
+        #print("num < mid")
         end = mid
         mid = (start+end)//2
-        return binary_tree(num,lst,start,end,mid)
+        return binary_tree(num,lst,start,mid-1)
 
 N = int(input(">> "))
 n_lst = sorted(list(map(int, input(">> ").split())))
 M = int(input(">> "))
 m_lst = list(map(int, input(">> ").split()))
-print(binary_tree(m_lst[0],n_lst,0,N-1,(N-1)//2))
+result = []
+for num in m_lst:
+    result.append(binary_tree(num,n_lst,0,N-1))
+print(' '.join(result))
