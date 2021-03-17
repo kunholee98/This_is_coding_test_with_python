439142414453829f5ff21718b1af44cd0bc991be
kunholee_98 <dlrjsgh750@naver.com>
2021-03-17 23:46:56 +0900

    개미 전사

--- chapter8/ant_soldier.py
+++ chapter8/ant_soldier.py
@@ -1,6 +1,10 @@
 # 개미 전사: N size의 식량창고 리스트가 주어질 때, 인접한 식량 창고를 털 수 없는 조건에서 얼마나 많은 식량을 훔칠 수 있겠는가?
 N = int(input(">> "))   # 3~100
-K = list(map(int,input(">> "))) # 0~1000
+K = list(map(int,input(">> ").split())) # 0~1000
 sum = 0
-while i > len(K):
-    if sum + K[i]   # 모든 인덱스를 동시에 고려해야 풀만할 것같은데.. 왜 책은 점화식이 저렇게 생겼지.. 난 이해가 안되네..
+for i in range(len(K)):
+    if i == 0 or i == 1:
+        continue
+    else:
+        K[i] = max(K[i]+K[i-2], K[i-1])
+print(K[-1])
