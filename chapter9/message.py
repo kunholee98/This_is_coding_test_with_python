8d94044588d0cdf5810f3f9086c6f0ec3e9dfe3f
kunholee_98 <dlrjsgh750@naver.com>
2021-03-26 21:43:49 +0900

    전보

--- chapter9/message.py
+++ chapter9/message.py
@@ -13,9 +13,32 @@ way[C] = 0
 visited = [False]*(N+1)
 visited[C] = True
 
+# C도시에서 갈 수 있는 지점들이 i[0]까지의 거리 i[1]을 way 리스트에 우선 입력한다. (초기값)
 for i in message[C]:
     way[i[0]] = i[1]
-for i in range(N-1):
-    for j in message:
 
+# N-1번 반복해서 실행하면 모든 지점들을 확인할 수 있고
+for i in range(N-1):
+    # 각 반복마다 message의 모든 값을 불러온다.
+    min_value = 1001
+    index = 0
+    for j in range(1,N+1):
+        if way[j] < min_value and not visited[j]:
+            min_value = way[i]
+            index = j
+    visited[index] = True
+    for k in message[index]:
+        cost = way[index] + k[1]
+        if cost < way[k[0]]:
+            way[k[0]] = cost
+cnt = 0
+maximum = 0
+for i in range(1,N+1):
+    if way[i] == 1001 or way[i] == 0:
+        pass
+    else:
+        cnt += 1
+        maximum = max(maximum,way[i])
+print(cnt, maximum)
+        
 # 다익스트라 알고리즘으로 푸는 중입니다.
